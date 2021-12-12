e42490012c9e424ec3fe43d80c1651cf8d47d794
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2021-12-12 18:13:37 +0100

    Separate markdown rendering

--- src/Frontend/Game.elm
+++ src/Frontend/Game.elm
@@ -1,18 +1,16 @@
 module Frontend.Game exposing (view)
 
 import Dict
-import Element.WithContext as Element exposing (Orientation(..), alignBottom, alignTop, centerX, centerY, column, el, fill, fillPortion, height, image, paragraph, px, row, text, width, wrappedRow)
+import Element.WithContext as Element exposing (Orientation(..), alignBottom, alignTop, centerX, centerY, column, el, fill, fillPortion, height, image, px, row, text, width, wrappedRow)
 import Element.WithContext.Background as Background
-import Element.WithContext.Border as Border
 import Element.WithContext.Font as Font
 import Element.WithContext.Input as Input
 import Frontend.Common
+import Frontend.GameMarkdown
 import Frontend.GameTheme as Theme exposing (Attribute, Element)
-import Html
 import Html.Attributes
 import MapPixels
 import Markdown.Block exposing (ListItem(..), Task(..))
-import Markdown.Html
 import Markdown.Parser
 import Markdown.Renderer
 import Model exposing (ChatHistory, Choice, City, Data, GameModel(..), Id, MapModel, MenuModel, Next(..), Person, Quiz, SharedGameModel, TalkingModel, mapSize)
@@ -629,156 +627,12 @@ semiBox attrs =
 -- Markdown renderer
 
 
-elmUiRenderer : Markdown.Renderer.Renderer (Element msg)
-elmUiRenderer =
-    { heading = heading
-    , paragraph = paragraph [ Theme.spacing ]
-    , thematicBreak = Element.none
-    , text = Element.text
-    , strong = \content -> Element.row [ Font.bold ] content
-    , emphasis = \content -> Element.row [ Font.italic ] content
-    , strikethrough = \content -> Element.row [ Font.strike ] content
-    , codeSpan = code
-    , link =
-        \{ destination } body ->
-            Element.newTabLink
-                [ Element.htmlAttribute (Html.Attributes.style "display" "inline-flex") ]
-                { url = destination
-                , label =
-                    Element.paragraph
-                        [ Font.color (Element.rgb255 0 0 255)
-                        ]
-                        body
-                }
-    , hardLineBreak = Html.br [] [] |> Element.html
-    , image =
-        \image ->
-            Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
-    , blockQuote =
-        \children ->
-            Element.column
-                [ Theme.borderWidthEach
-                    { top = False
-                    , right = False
-                    , bottom = False
-                    , left = True
-                    }
-                , Theme.padding
-                , Border.color (Element.rgb255 145 145 145)
-                , Background.color (Element.rgb255 245 245 245)
-                ]
-                children
-    , unorderedList =
-        \items ->
-            column [ Theme.spacing ]
-                (items
-                    |> List.map
-                        (\(ListItem task children) ->
-                            row [ Theme.spacing ]
-                                [ row
-                                    [ alignTop ]
-                                    ((case task of
-                                        IncompleteTask ->
-                                            Input.defaultCheckbox False
-
-                                        CompletedTask ->
-                                            Input.defaultCheckbox True
-
-                                        NoTask ->
-                                            text "•"
-                                     )
-                                        :: text " "
-                                        :: children
-                                    )
-                                ]
-                        )
-                )
-    , orderedList =
-        \startingIndex items ->
-            column [ Theme.spacing ]
-                (items
-                    |> List.indexedMap
-                        (\index itemBlocks ->
-                            row [ Theme.spacing ]
-                                [ row [ alignTop ]
-                                    (text (String.fromInt (index + startingIndex) ++ " ") :: itemBlocks)
-                                ]
-                        )
-                )
-    , codeBlock = codeBlock
-    , html = Markdown.Html.oneOf []
-    , table = column []
-    , tableHeader = column []
-    , tableBody = column []
-    , tableRow = row []
-    , tableHeaderCell = \_ children -> paragraph [] children
-    , tableCell = \_ children -> paragraph [] children
-    }
-
-
-code : String -> Element msg
-code snippet =
-    Element.el
-        [ Background.color (Element.rgba 0 0 0 0.04)
-        , Theme.borderRounded
-        , Theme.padding
-        , Font.family [ Font.monospace ]
-        ]
-        (Element.text snippet)
-
-
-codeBlock : { body : String, language : Maybe String } -> Element msg
-codeBlock details =
-    Element.el
-        [ Background.color (Element.rgba 0 0 0 0.03)
-        , Element.htmlAttribute (Html.Attributes.style "white-space" "pre")
-        , Theme.padding
-        , Font.family [ Font.monospace ]
-        ]
-        (Element.text details.body)
-
-
-heading :
-    { level : Markdown.Block.HeadingLevel
-    , rawText : String
-    , children : List (Element msg)
-    }
-    -> Element msg
-heading { level, rawText, children } =
-    Element.paragraph
-        [ case level of
-            Markdown.Block.H1 ->
-                Theme.fontSize 1.8
-
-            Markdown.Block.H2 ->
-                Theme.fontSize 1.2
-
-            _ ->
-                Theme.fontSize 1
-        , Font.bold
-        , Font.family [ Font.typeface "Montserrat" ]
-        , Element.htmlAttribute
-            (Html.Attributes.attribute "name" (rawTextToId rawText))
-        , Element.htmlAttribute
-            (Html.Attributes.id (rawTextToId rawText))
-        ]
-        children
-
-
-rawTextToId : String -> String
-rawTextToId rawText =
-    rawText
-        |> String.split " "
-        |> String.join "-"
-        |> String.toLower
-
-
 viewMarked : List (Attribute msg) -> String -> Element msg
 viewMarked attrs input =
     input
         |> String.replace "  " "\n\n"
         |> Markdown.Parser.parse
         |> Result.mapError (\_ -> "Parsing error")
-        |> Result.andThen (Markdown.Renderer.render elmUiRenderer)
+        |> Result.andThen (Markdown.Renderer.render Frontend.GameMarkdown.elmUiRenderer)
         |> Result.map (column (Theme.spacing :: attrs))
         |> Result.withDefault (text input)
---
+++ src/Frontend/GameMarkdown.elm
@@ -0,0 +1,159 @@
+module Frontend.GameMarkdown exposing (code, codeBlock, elmUiRenderer, heading, rawTextToId)
+
+import Element.WithContext as Element exposing (Orientation(..), alignTop, column, image, paragraph, row, text)
+import Element.WithContext.Background as Background
+import Element.WithContext.Border as Border
+import Element.WithContext.Font as Font
+import Element.WithContext.Input as Input
+import Frontend.GameTheme as Theme exposing (Element)
+import Html
+import Html.Attributes
+import Markdown.Block exposing (ListItem(..), Task(..))
+import Markdown.Html
+import Markdown.Renderer
+import Model exposing (GameModel(..), Next(..))
+import Types exposing (GameMsg(..), OuterGameModel(..))
+
+
+elmUiRenderer : Markdown.Renderer.Renderer (Element msg)
+elmUiRenderer =
+    { heading = heading
+    , paragraph = paragraph [ Theme.spacing ]
+    , thematicBreak = Element.none
+    , text = Element.text
+    , strong = \content -> Element.row [ Font.bold ] content
+    , emphasis = \content -> Element.row [ Font.italic ] content
+    , strikethrough = \content -> Element.row [ Font.strike ] content
+    , codeSpan = code
+    , link =
+        \{ destination } body ->
+            Element.newTabLink
+                [ Element.htmlAttribute (Html.Attributes.style "display" "inline-flex") ]
+                { url = destination
+                , label =
+                    Element.paragraph
+                        [ Font.color (Element.rgb255 0 0 255)
+                        ]
+                        body
+                }
+    , hardLineBreak = Html.br [] [] |> Element.html
+    , image =
+        \image ->
+            Element.image [ Element.width Element.fill ] { src = image.src, description = image.alt }
+    , blockQuote =
+        \children ->
+            Element.column
+                [ Theme.borderWidthEach
+                    { top = False
+                    , right = False
+                    , bottom = False
+                    , left = True
+                    }
+                , Theme.padding
+                , Border.color (Element.rgb255 145 145 145)
+                , Background.color (Element.rgb255 245 245 245)
+                ]
+                children
+    , unorderedList =
+        \items ->
+            column [ Theme.spacing ]
+                (items
+                    |> List.map
+                        (\(ListItem task children) ->
+                            row [ Theme.spacing ]
+                                [ row
+                                    [ alignTop ]
+                                    ((case task of
+                                        IncompleteTask ->
+                                            Input.defaultCheckbox False
+
+                                        CompletedTask ->
+                                            Input.defaultCheckbox True
+
+                                        NoTask ->
+                                            text "•"
+                                     )
+                                        :: text " "
+                                        :: children
+                                    )
+                                ]
+                        )
+                )
+    , orderedList =
+        \startingIndex items ->
+            column [ Theme.spacing ]
+                (items
+                    |> List.indexedMap
+                        (\index itemBlocks ->
+                            row [ Theme.spacing ]
+                                [ row [ alignTop ]
+                                    (text (String.fromInt (index + startingIndex) ++ " ") :: itemBlocks)
+                                ]
+                        )
+                )
+    , codeBlock = codeBlock
+    , html = Markdown.Html.oneOf []
+    , table = column []
+    , tableHeader = column []
+    , tableBody = column []
+    , tableRow = row []
+    , tableHeaderCell = \_ children -> paragraph [] children
+    , tableCell = \_ children -> paragraph [] children
+    }
+
+
+code : String -> Element msg
+code snippet =
+    Element.el
+        [ Background.color (Element.rgba 0 0 0 0.04)
+        , Theme.borderRounded
+        , Theme.padding
+        , Font.family [ Font.monospace ]
+        ]
+        (Element.text snippet)
+
+
+codeBlock : { body : String, language : Maybe String } -> Element msg
+codeBlock details =
+    Element.el
+        [ Background.color (Element.rgba 0 0 0 0.03)
+        , Element.htmlAttribute (Html.Attributes.style "white-space" "pre")
+        , Theme.padding
+        , Font.family [ Font.monospace ]
+        ]
+        (Element.text details.body)
+
+
+heading :
+    { level : Markdown.Block.HeadingLevel
+    , rawText : String
+    , children : List (Element msg)
+    }
+    -> Element msg
+heading { level, rawText, children } =
+    Element.paragraph
+        [ case level of
+            Markdown.Block.H1 ->
+                Theme.fontSize 1.8
+
+            Markdown.Block.H2 ->
+                Theme.fontSize 1.2
+
+            _ ->
+                Theme.fontSize 1
+        , Font.bold
+        , Font.family [ Font.typeface "Montserrat" ]
+        , Element.htmlAttribute
+            (Html.Attributes.attribute "name" (rawTextToId rawText))
+        , Element.htmlAttribute
+            (Html.Attributes.id (rawTextToId rawText))
+        ]
+        children
+
+
+rawTextToId : String -> String
+rawTextToId rawText =
+    rawText
+        |> String.split " "
+        |> String.join "-"
+        |> String.toLower
